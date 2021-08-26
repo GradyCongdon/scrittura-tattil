9b27cd4559182f35b55549bf88f322d86b5eb439
Grady Congdon <gradycongdon@gmail.com>
2021-08-25 20:01:51 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
+import { KeyboardEvent, useState } from 'react';
 import './App.css';
+import { message } from './message';
+
+interface Letter {
+  shape: string;
+  text: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+const LetterShape = ({ shape, text, x, y, width, height }: Letter) => (
+  <span style={{
+    position: 'absolute',
+    top: y - 4,
+    left: x + 1,
+  }}>
+    {text}
+  </span>
+)
+
+const width = 10;
+const height = 10;
+
+// const isLetter = (key: string) => key.match(/^[a-zA-Z]$/);
+const isLetter = (key: string) => key.length === 1;
+
+const getLocalState = (): Letter[] => {
+  const localStore = localStorage.getItem('state');
+  return localStore ? JSON.parse(localStore) : null;
+}
 
 function App() {
+  const localState = getLocalState();
+  const state = localState || message;
+  const [letters, setLetters] = useState(state);
+  const [isDefault, setIsDefault] = useState(!localState);
+  const [shape, setShape] = useState('');
+  ;
+
+
+  const addLetter = (char: string) => {
+    const { mouse } = window as any;
+    const letter = {
+      shape,
+      width,
+      height,
+      x: mouse.x,
+      y: mouse.y,
+      text: char,
+    };
+    setShape('');
+    if (isDefault) {
+      setIsDefault(false);
+      setLetters([letter]);
+    } else {
+      setLetters([...letters, letter]);
+    }
+  }
+
+  const handleKey = (e: KeyboardEvent) => {
+    const { key } = e;
+    if (!isLetter(key)) return;
+    addLetter(key);
+    localStorage.setItem('state', JSON.stringify(letters));
+  }
+
+  const inputStyle = {
+    width: '100vw',
+    height: '100vh',
+    position: 'fixed' as any,
+    visbility: 'hidden',
+    outline: 'none',
+    appearance: 'none' as any,
+    border: 'none',
+    color: 'transparent'
+  }
+
+  const buttonStyle = {
+    position: 'fixed' as any,
+    top: 0,
+    right: 0,
+    appearance: 'none' as any,
+    border: 'none',
+    backgroundColor: 'white' as any,
+    color: 'rgba(255, 0, 0, .2)',
+    outline: 'none',
+  }
+
+  const reset = () => {
+    setIsDefault(true);
+    setLetters(message);
+    localStorage.setItem('state', JSON.stringify([]));
+    document.getElementById('board')?.focus();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <input id="board" type="text" onKeyDown={handleKey} autoFocus style={inputStyle} />
+      {letters.map((letter, i) => <LetterShape key={i} {...letter} />)}
+      <button onClick={reset} style={buttonStyle}>x</button>
+    </main>
   );
 }
 
---
+++ src/message.tsx
@@ -0,0 +1,234 @@
+export const message = [
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 462,
+    "y": 314,
+    "text": "m"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 476,
+    "y": 311,
+    "text": "o"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 493,
+    "y": 313,
+    "text": "v"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 510,
+    "y": 307,
+    "text": "e"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 553,
+    "y": 313,
+    "text": "m"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 553,
+    "y": 313,
+    "text": "o"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 568,
+    "y": 312,
+    "text": "o"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 586,
+    "y": 309,
+    "text": "u"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 600,
+    "y": 312,
+    "text": "s"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 612,
+    "y": 311,
+    "text": "e"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 658,
+    "y": 311,
+    "text": "a"
+  },
+  // {
+  //   "shape": "",
+  //   "width": 10,
+  //   "height": 10,
+  //   "x": 658,
+  //   "y": 311,
+  //   "text": "n"
+  // },
+  // {
+  //   "shape": "",
+  //   "width": 10,
+  //   "height": 10,
+  //   "x": 658,
+  //   "y": 311,
+  //   "text": "d"
+  // },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 674,
+    "y": 311,
+    "text": "a"
+  },
+  // {
+  //   "shape": "",
+  //   "width": 10,
+  //   "height": 10,
+  //   "x": 664,
+  //   "y": 312,
+  //   "text": "x"
+  // },
+  // {
+  //   "shape": "",
+  //   "width": 10,
+  //   "height": 10,
+  //   "x": 658,
+  //   "y": 306,
+  //   "text": "x"
+  // },
+  // {
+  //   "shape": "",
+  //   "width": 10,
+  //   "height": 10,
+  //   "x": 658,
+  //   "y": 306,
+  //   "text": "x"
+  // },
+  // {
+  //   "shape": "",
+  //   "width": 10,
+  //   "height": 10,
+  //   "x": 658,
+  //   "y": 310,
+  //   "text": "x"
+  // },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 655,
+    "y": 312,
+    "text": "x"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 656,
+    "y": 314,
+    "text": "x"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 663,
+    "y": 316,
+    "text": "x"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 646,
+    "y": 318,
+    "text": "x"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 688,
+    "y": 312,
+    "text": "n"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 704,
+    "y": 309,
+    "text": "d"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 736,
+    "y": 314,
+    "text": "t"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 751,
+    "y": 312,
+    "text": "y"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 766,
+    "y": 311,
+    "text": "p"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 780,
+    "y": 311,
+    "text": "e"
+  },
+  {
+    "shape": "",
+    "width": 10,
+    "height": 10,
+    "x": 652,
+    "y": 305,
+    "text": "x"
+  }
+];
